7bf3d50c86ec481a66fbeb98e30d9ffb18beb394
eduardo <eduvela_94@hotmail.com>
2021-01-09 22:48:54 +0100

    server running port 5000

--- index.js
+++ index.js
@@ -2,23 +2,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-const HOST = '17.0.0.1';
-const PORT = '5000';
+const HOST = '127.0.0.1';
+const PORT = 5000;
 
 //Build the app
-const app = express();
+const server = express();
+server.use(bodyParser.json());
 
 //This is similar to Node.js' http module
 //const app = http.CreateServer()
 
-//Add some middleware 
-app.use(req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world!');
-});
+//Add some middleware
+// app.use(req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello world!');
+// });
 
 // Start it up!
-app.listen(PORT, () => console.log(`Serving on port ${PORT}🤖`));
+
+const noEndpointHandler = (request, response, next) => {
+  console.log('touching an endpoint');
+};
+
+server.get('*', noEndpointHandler);
+
+server.listen(PORT, () => console.log(`Serving on port ${PORT}🤖`));
 
 // MVC
 //Model (Modelos)
@@ -26,4 +34,4 @@ app.listen(PORT, () => console.log(`Serving on port ${PORT}🤖`));
 //Controllers (Controladores)
 
 //Body parser
-//Transmitir data cliente -> servidor utilizando URLs 
+//Transmitir data cliente -> servidor utilizando URLs
